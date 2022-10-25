db5305416f8e527b5955e09dc42edffbd4055545
emsecrist <emsecrist@users.noreply.github.com>
2022-10-25 21:09:18 +0000

    Reformat terraform files
    Signed-off-by: emsecrist <emsecrist@users.noreply.github.com>

--- main.tf
+++ main.tf
@@ -1,21 +1,21 @@
 terraform {
   required_providers {
     aws = {
-      source = "hashicorp/aws"
-      version = 	"4.15.1"
+      source  = "hashicorp/aws"
+      version = "4.15.1"
     }
   }
 }
 
-  provider "aws" {
-    region = "us-west-2"
-  }
+provider "aws" {
+  region = "us-west-2"
+}
 
-  resource "aws_instance" "eric_example" {
-    ami		  = "ami-ebc42a93"
-    instance_type = "t2.micro"
-  
-    tags = {
-      Name = "eric-example"
-    }
+resource "aws_instance" "eric_example" {
+  ami           = "ami-ebc42a93"
+  instance_type = "t2.micro"
+
+  tags = {
+    Name = "eric-example"
+  }
 }
