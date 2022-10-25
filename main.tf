6a83537b91bc3edca015d2a8adfa8d4649fb6587
Eric Secrist <eric.secrist@gmail.com>
2022-10-25 12:58:55 -0600

    Add GH action, initial commit

---
+++ main.tf
@@ -0,0 +1,21 @@
+terraform {
+  required_providers {
+    aws = {
+      source = "hashicorp/aws"
+      version = "4.15.1"
+    }
+  }
+}
+
+  provider "aws" {
+    region = "us-west-2"
+  }
+
+  resource "aws_instance" "eric_example" {
+    ami		  = "ami-ebc42a93"
+    instance_type = "t2.micro"
+  
+    tags = {
+      Name = "eric-example"
+    }
+}
